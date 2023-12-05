cc58561b3d32bef613a4a8fb0041b5e70f25a0b3
Lextuga007 <zoe.turner3@nhs.net>
2023-12-05 15:17:31 +0000

    Changed to url link because that's so much better :poop:

--- capacity_ae_analysis.R
+++ capacity_ae_analysis.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 # Plot a chart
 
-capacity_ae <- read_csv("data/capacity_ae.csv")
+capacity_ae <- read_csv(url("https://raw.githubusercontent.com/nhs-r-community/intro_r_data/main/capacity_ae.csv"))
 
 ggplot(data = capacity_ae,
        aes(x = dcubicles, y = dwait)) +
